2678c9adfeb1acac2a4241704dcb5462a7325da2
maoyongjun <H7108464@foxconn.com>
2018-05-24 14:56:23 +0800

    commit

---
+++ TencentWebApi/HttpUtil/HttpUtil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpUtil
+{
+    public class HttpUtil
+    {
+        private String httpPost(String url, String postDataStr) {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = Encoding.UTF8.GetByteCount(postDataStr);
+            request.CookieContainer = cookie;
+            Stream myRequestStream= request.GetRequestStream();
+            StreamWriter myStreamWriter = new StreamWriter(myRequestStream,Encoding.GetEncoding("gb2312"));
+            myStreamWriter.Write(postDataStr);
+            myStreamWriter.Close();
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            response.Cookies = cookie.GetCookies(response.ResponseUri);
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myRequestStream,Encoding.GetEncoding("utf-8"));
+            String retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+            return retString;
+        }
+
+        private String Http
+
+    }
+}
